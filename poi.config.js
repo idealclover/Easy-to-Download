57a85fd106568b636e816c0bb8c864303aee96f2
idealclover <idealclover@live.com>
2021-08-15 21:42:10 +0800

    :bug: fix bug

--- poi.config.js
+++ poi.config.js
@@ -3,7 +3,8 @@ require("dotenv").config()
 let pageData = require("./config.json")
 
 const env = process.env
-const publicPath = env.NODE_ENV === "production" ? "env.PUBLIC_PATH" : "/"
+const publicPath = "/"
+// const publicPath = env.NODE_ENV === "production" ? "env.PUBLIC_PATH" : "/"
 
 console.log(`PUBLIC_PATH: ${publicPath}`)
 
