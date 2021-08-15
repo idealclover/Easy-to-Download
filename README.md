4e601e2714ae09ad1fdd6db2a06b22f449540db4
idealclover <idealclover@live.com>
2021-08-15 21:09:06 +0800

    :dog: not yet finished

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # app-download
 
-[![Build Status](https://travis-ci.com/FEMessage/app-download.svg?branch=master)](https://travis-ci.com/FEMessage/app-download)
-[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](https://github.com/FEMessage/app-download/pulls)
-[![Automated Release Notes by gren](https://img.shields.io/badge/%F0%9F%A4%96-release%20notes-00B2EE.svg)](https://github-tools.github.io/github-release-notes/)
-
-
 - PC显示效果
 
 ![app-download-pc](https://i.screenshot.net/elnz9uj)
--- index.js
+++ index.js
@@ -1,75 +1,69 @@
-import "./style/bulma.css"
-import "./style/common.less"
-import Qrcode from "qrcode"
-import * as $ from "./lib/zepto.js"
+import Qrcode from "qrcode";
+import * as $ from "./lib/zepto.js";
 
-const $tips = $(".wechat-tips")
-const $card = $(".download-card")
-const $qrcode = $(".insert-qrcode")
-const ua = navigator.userAgent
-const isWechat = /micromessenger/i.test(ua)
-const isDingTalk = /DingTalk/i.test(ua)
+const $tips = $(".wechat-tips");
+// const $card = $(".download-card")
+const $link = $(".download-link");
+const $qrcode = $(".insert-qrcode");
+const ua = navigator.userAgent;
+const isWechat = /micromessenger/i.test(ua);
+const isDingTalk = /DingTalk/i.test(ua);
 
 function downloadApp(link) {
   // 使用location.assign 兼容 ios safari跳转
-  link && location.assign(link)
+  $link.attr("href", link);
+  if ($.os.ios) link && location.assign(link);
 }
 
 function getData(node) {
-  const data = {}
-  node.forEach((item) => {
-    const {platform, link} = item.dataset
-    data[platform.toLowerCase()] = link
-  })
-  return data
+  const data = {
+    android: node[0].dataset.android,
+    ios: node[0].dataset.ios,
+  };
+  return data;
 }
 
 function getNowLink(downloadLinks) {
-  const ANDROID = "android"
-  const IOS = "ios"
+  const ANDROID = "android";
+  const IOS = "ios";
 
-  return $.os.android ? downloadLinks[ANDROID] :
-          $.os.ios ? downloadLinks[IOS] :
-          ''
+  return $.os.android
+    ? downloadLinks[ANDROID]
+    : $.os.ios
+    ? downloadLinks[IOS]
+    : "";
 }
 
 function generateQrcode(text) {
   return new Promise((resolve, reject) => {
-    Qrcode.toDataURL(text, {errorCorrectionLevel: "L"}, function(err, url) {
+    Qrcode.toDataURL(text, { errorCorrectionLevel: "L" }, function(err, url) {
       if (url) {
-        resolve(url)
+        resolve(url);
       } else {
-        reject(err)
+        reject(err);
       }
-    })
-  })
+    });
+  });
 }
 
 function init() {
   if (isWechat || ($.os.ios && isDingTalk)) {
-    return $tips.show()
+    return $tips.show();
   }
 
-  const downloadLinks = getData($card)
-
-  const link = getNowLink(downloadLinks)
-
-  downloadApp(link)
-
-  $card.on("click", function(e) {
-    const $this = $(this)
-    const link = $this.attr("data-link")
-    downloadApp(link)
-  })
+  const downloadLinks = getData($link);
+  const link = getNowLink(downloadLinks);
+  downloadApp(link);
 
   if (!$.os.phone) {
     generateQrcode(location.href)
-      .then(url => {
+      .then((url) => {
         $qrcode.forEach(function(item) {
-          $(item).attr("src", url)
-        })
-      }).catch(console.error)
+          $(item).attr("src", url);
+        });
+      })
+      .catch(console.error);
   }
 }
 
-init()
+init();
